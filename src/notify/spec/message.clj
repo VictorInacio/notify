f95dec29db08da8f863d459917aad27b3a604533
Victor Inacio <victorinacio@gmail.com>
2023-08-03 13:59:31 -0300

    spec detailed

--- src/notify/spec/message.clj
+++ src/notify/spec/message.clj
@@ -1,19 +1,70 @@
 (ns notify.spec.message
   (:require [clojure.spec.alpha :as s]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Categories                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def category? #{:sports :finance :movies})
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Channels                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def channel? #{:sms :email :push-notification})
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Messages                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(s/def :message/slug string?)
+
+(s/def :message/content string?)
+
+(s/valid? inst? (s/inst-in #inst "1970" #inst "9999"))
+
+(s/def :message/publication-date inst?)
+
+(s/def :message/category category?)
 
 (s/def :publicated/message (s/keys :req [:message/slug
                                          :message/content
-                                         :message/category
-                                         :message/publication-date]
-                                   :opt [:message/author]))
+                                         :message/publication-date
+                                         :message/category]))
 
-(s/def :user/entity (s/keys :req [:user/id
-                                  :user/name
-                                  :user/email
-                                  :user/subscribed
-                                  :user/channels]
-                            :opt [:user/phone-number]))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Users                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(s/def :user/id int?)
+
+(s/def :user/name string?)
+
+(s/def :user/email string?)
+
+(s/def :user/subscribed (s/coll-of :message/category))
+
+(s/def :user/channels (s/coll-of :notification/channel))
+
+(s/def :user/user (s/keys :req [:user/id
+                                :user/name
+                                :user/email
+                                :user/subscribed
+                                :user/channels]
+                          :opt [:user/phone-number]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Notifications                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(s/def :notification/id int?)
+
+(s/def :notification/user :user/user)
+
+(s/def :notification/channel channel?)
+
+(s/def :user/email string?)
 
 (s/def :sent/notification (s/keys :req [:notification/id
                                         :notification/user
@@ -21,6 +72,4 @@
                                         :message/slug
                                         :message/content
                                         :message/category
-                                        :message/publication-date
-                                        ]
-                                  :opt [:message/author]))
+                                        :message/publication-date]))
